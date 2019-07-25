fbc1a64f860c3ecdfb582e7288c4228c912cc81d
Savannah C <sgcorrell@gmail.com>
2019-07-25 04:07:07 +0000

    Automatically backed up by Learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -5,7 +5,7 @@ class ArtistsController < ApplicationController
   end
   
   def create
-    @artist = Artist.new(params.require)
+    @artist = Artist.new(params.require(:artist))
   end
   
   def show
