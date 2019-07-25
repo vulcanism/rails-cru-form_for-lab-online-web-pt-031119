7eb1df2f41ee40745010bae8aad70e0f85708c97
Savannah C <sgcorrell@gmail.com>
2019-07-25 04:15:36 +0000

    Automatically backed up by Learn

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -4,6 +4,20 @@ class GenresController < ApplicationController
     
   end
   
+  def create
+    
+  end
   
+  def show
+    
+  end
+  
+  def edit
+    
+  end
+  
+  def update
+    
+  end
   
 end
