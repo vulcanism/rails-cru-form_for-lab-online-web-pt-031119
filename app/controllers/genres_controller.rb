ec44515f1ef931fa4bceee824407f5fcc2f73002
Savannah C <sgcorrell@gmail.com>
2019-07-25 04:17:27 +0000

    Automatically backed up by Learn

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -6,6 +6,8 @@ class GenresController < ApplicationController
   
   def create
     @genre = Genre.new(params.require(:genre).permit(:name))
+    @genre.save
+    redirect_to genre_path(@genre)
   end
   
   def show
