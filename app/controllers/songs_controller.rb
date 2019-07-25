dade71e6fcdedb8b48edbe3c6c105d8ff971ef92
Savannah C <sgcorrell@gmail.com>
2019-07-25 04:25:03 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -21,6 +21,8 @@ class SongsController < ApplicationController
   def update
     @song = Song.find(params[:id])
     @song.update(params.require(:song).permit(:name, :artist_id, :genre_id))
+    @song.save
+    redirect_to song_path(@song)
   end
   
   def index
